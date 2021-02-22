a392913fe3aa1ca5f1332a49c58c6b8e0ddf7305
luojinxu520 <you@example.com>
2021-02-22 13:13:08 +0800

    All: qiankun based

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div id="app">
+    <header>I am a header</header>
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <el-menu :router="true" mode="horizontal">
+        <el-menu-item index="/">Home</el-menu-item>
+        <el-menu-item index="/vue">Vue</el-menu-item>
+        <el-menu-item index="/react">React</el-menu-item>
+      </el-menu>
     </div>
-    <router-view/>
+    <router-view />
+    <div id="vue"></div>
+    <div id="react"></div>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,37 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import { registerMicroApps, start } from 'qiankun'
 
-Vue.config.productionTip = false
+Vue.use(ElementUI)
+
+const apps = [
+  {
+    name: 'vueApp',
+    entry: process.env.NODE_ENV === 'production' ? 'http://49.234.62.202:10000' : 'http://localhost:10000',
+    fetch: '',
+    container: '#vue',
+    activeRule: '/vue',
+    props: {
+      data: [1, 2, 3, 4, 5]
+    }
+  },
+  {
+    name: 'reactApp',
+    entry: process.env.NODE_ENV === 'production' ? 'http://49.234.62.202:20000' : 'http://localhost:20000',
+    fetch: '',
+    container: '#react',
+    activeRule: '/react',
+    props: {
+      data: [1, 2, 3, 4, 5]
+    }
+  }
+]
+
+registerMicroApps(apps)
+start()
 
 new Vue({
   router,
